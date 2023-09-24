0f7788595ebed5de93e9fbc9869e03fa8b63147e
Suzan Shrestha <sthabikash11@gmail.com>
2023-09-24 23:26:22 +1000

    added code file

---
+++ code1.1.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+
+#loading csv file
+calendar_db = pd.read_csv('calendar_dec18.csv')
+listings_db = pd.read_csv('listings_dec18.csv')
+reviews_db = pd.read_csv('reviews_dec18.csv')
+
+
+plt.show()
+
+
